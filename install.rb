7cd33c33d5c71abc77fdd765fb6bcc490b2e1e52
Miquel Sabaté <mikisabate@gmail.com>
2011-10-06 20:32:04 +0200

    Check for the wirble gem before installing

--- install.rb
+++ install.rb
@@ -4,6 +4,14 @@
 
 #!/usr/bin/env ruby
 
+# Check for the wirble gem since the .irbrc file requires
+# this gem to successfully work.
+begin
+  require 'wirble'
+rescue LoadError
+  puts 'The .irbrc file requires wirble and you haven\'t this gem installed!'
+end
+
 # You can change your home directory ;)
 home = ARGV[0]
 home ||= '$HOME'
