a7a96a40351676f37cdef12fb46d5646db207dcf
Miquel Sabaté Solà <msabate@suse.com>
2019-09-16 16:52:51 +0200

    emacs: added abbrev into the mix
    I'm also including some other packages such as dired-x.
    
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

---
+++ .emacs.d/abbrevs.el
@@ -0,0 +1,6 @@
+(define-abbrev-table 'global-abbrev-table '(
+    ("abbout" "about" nil 0)
+	("afaict" "as far as I can tell" nil 0)
+	("btw" "by the way" nil 0)
+    ("diba" "Diputació de Barcelona" nil 0)
+    ))
